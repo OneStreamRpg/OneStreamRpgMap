10033549fd0a80f7e6c4da585f53212e51652bec
Julian Paule <nicer.typ@gmail.com>
2025-12-08 18:53:30 +0100

    _

---
+++ Map Design/arenaGround.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="arenaGround" tilewidth="16" tileheight="16" tilecount="638" columns="29">
+ <image source="../tileset/arenaGround.png" width="469" height="353"/>
+</tileset>
---
+++ Map Design/front.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="front" tilewidth="16" tileheight="16" tilecount="136" columns="17">
+ <image source="../../../../Downloads/arena/front.png" width="282" height="132"/>
+</tileset>
