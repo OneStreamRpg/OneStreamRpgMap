3df349c29bc1877b1225dcf62078849496197e87
BrunoZoller <33461679+BrunoZoller@users.noreply.github.com>
2025-12-11 12:53:57 +0100

    general change

---
+++ Map Design/10bc95bd-dd79-4db1-815c-e5400194a054.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="10bc95bd-dd79-4db1-815c-e5400194a054" tilewidth="16" tileheight="16" tilecount="250" columns="25">
+ <image source="../../../GitHub/oneStreamRpg2Client/sprites/s807/10bc95bd-dd79-4db1-815c-e5400194a054.png" width="400" height="161"/>
+</tileset>
---
+++ Map Design/4392c52d-aace-406a-b975-38b6461be819.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="4392c52d-aace-406a-b975-38b6461be819" tilewidth="16" tileheight="16" tilecount="672" columns="32">
+ <image source="../../../GitHub/oneStreamRpg2Client/sprites/summer_forest/4392c52d-aace-406a-b975-38b6461be819.png" width="512" height="336"/>
+</tileset>
