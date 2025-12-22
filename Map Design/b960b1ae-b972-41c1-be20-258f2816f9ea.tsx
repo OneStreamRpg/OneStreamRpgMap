053bcf5928c570698bf481c2a87887532398b2c8
Julian Paule <nicer.typ@gmail.com>
2025-12-22 14:21:14 +0100

    imported missing files

--- Map Design/b960b1ae-b972-41c1-be20-258f2816f9ea.tsx
+++ Map Design/b960b1ae-b972-41c1-be20-258f2816f9ea.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="b960b1ae-b972-41c1-be20-258f2816f9ea" tilewidth="16" tileheight="16" tilecount="638" columns="29">
- <image source="../../../GitHub/oneStreamRpg2Client/sprites/b111b0fc_38ac_45c9_81a6_515629ade057/b960b1ae-b972-41c1-be20-258f2816f9ea.png" width="469" height="353"/>
+<tileset version="1.10" tiledversion="1.11.2" name="b960b1ae-b972-41c1-be20-258f2816f9ea" tilewidth="16" tileheight="16" tilecount="250" columns="25">
+ <image source="../tileset/bridge.png" width="400" height="161"/>
 </tileset>
