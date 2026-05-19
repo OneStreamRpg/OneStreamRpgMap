e674fa82526bdc44cf67cee2a360b010ee82e04b
BrunoZoller <33461679+BrunoZoller@users.noreply.github.com>
2026-05-19 13:02:39 +0200

    added pond and shit

---
+++ Map Design/3bd7f2b0-1eaa-402a-9dc8-090f73698458.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="3bd7f2b0-1eaa-402a-9dc8-090f73698458" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="../../../Old Projects/GitHub/oneStreamRpg2Client/sprites/sFishingPond/3bd7f2b0-1eaa-402a-9dc8-090f73698458.png" width="128" height="92"/>
+</tileset>
---
+++ Map Design/pond.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pond" tilewidth="16" tileheight="16" tilecount="40" columns="8">
+ <image source="../../../Old Projects/GitHub/oneStreamRpg2Client/sprites/sFishingPond/3bd7f2b0-1eaa-402a-9dc8-090f73698458.png" width="128" height="92"/>
+</tileset>
