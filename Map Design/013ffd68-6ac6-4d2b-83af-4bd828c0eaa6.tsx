bf1bd83c934e6c8e46b651389882a32f57d041d8
BrunoZoller <33461679+BrunoZoller@users.noreply.github.com>
2026-07-01 15:26:06 +0200

    map update

---
+++ Map Design/013ffd68-6ac6-4d2b-83af-4bd828c0eaa6.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="013ffd68-6ac6-4d2b-83af-4bd828c0eaa6" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../../../Old Projects/GitHub/oneStreamRpg2Client/sprites/sGround/013ffd68-6ac6-4d2b-83af-4bd828c0eaa6.png" width="128" height="128"/>
+</tileset>
