53aea945fa085d494dc121860dbaa87f61bb9196
BrunoZoller <33461679+BrunoZoller@users.noreply.github.com>
2026-07-14 22:07:22 +0200

    internalized the spritesheet

--- Map Design/013ffd68-6ac6-4d2b-83af-4bd828c0eaa6.tsx
+++ Map Design/013ffd68-6ac6-4d2b-83af-4bd828c0eaa6.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="013ffd68-6ac6-4d2b-83af-4bd828c0eaa6" tilewidth="16" tileheight="16" tilecount="64" columns="8">
- <image source="../../../Old Projects/GitHub/oneStreamRpg2Client/sprites/sGround/013ffd68-6ac6-4d2b-83af-4bd828c0eaa6.png" width="128" height="128"/>
+<tileset version="1.10" tiledversion="1.11.2" name="013ffd68-6ac6-4d2b-83af-4bd828c0eaa6" tilewidth="16" tileheight="16" tilecount="2898" columns="28">
+ <image source="sprites/custom sprites by beezy tm/573bdfdf-dc88-44ed-97f9-946b62430e6d.png" width="448" height="192"/>
 </tileset>
--- Map Design/573bdfdf-dc88-44ed-97f9-946b62430e6d.tsx
+++ Map Design/573bdfdf-dc88-44ed-97f9-946b62430e6d.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="573bdfdf-dc88-44ed-97f9-946b62430e6d" tilewidth="16" tileheight="16" tilecount="336" columns="28">
- <image source="../../../Old Projects/GitHub/oneStreamRpg2Client/sprites/Gemini_Generated_Image_ebc2nwebc2nwebc2_1__1_/573bdfdf-dc88-44ed-97f9-946b62430e6d.png" width="448" height="192"/>
+ <image source="sprites/custom sprites by beezy tm/573bdfdf-dc88-44ed-97f9-946b62430e6d.png" width="448" height="192"/>
 </tileset>
